78af9af01cebfb63530bf4336ccac76c5e7a9d8e
geenafong <geenafong@gmail.com>
2018-09-08 17:00:55 -0700

    save

--- js/script.js
+++ js/script.js
@@ -59,44 +59,44 @@ $("input[name='js-frameworks']").click(function(){
   if($(this).is(":checked")){
     currentValue = currentValue + 100;
     $(".activities p").text("Total: $ " + currentValue);
-    $("[name='express']").prop('disabled', 'true');
+    $("[name='express']").prop('disabled', 'true').parent().css({"color":"gray"});
   } else {
     currentValue = currentValue - 100;
     $(".activities p").text("Total: $ " + currentValue);
-     $("[name='express']").removeAttr('disabled');
+     $("[name='express']").removeAttr('disabled').parent().css({"color":"black"});
     }
  });
  $("input[name='express']").click(function(){
   if($(this).is(":checked")){
     currentValue = currentValue + 100;
     $(".activities p").text("Total: $ " + currentValue);
-    $("[name='js-frameworks']").prop('disabled', 'true');
+    $("[name='js-frameworks']").prop('disabled', 'true').parent().css({"color":"gray"});
   } else {
     currentValue = currentValue - 100;
     $(".activities p").text("Total: $ " + currentValue);
-     $("[name='js-frameworks']").removeAttr('disabled');
+     $("[name='js-frameworks']").removeAttr('disabled').parent().css({"color":"black"});
     }
  });
  $("input[name='js-libs']").click(function(){
   if($(this).is(":checked")){
     currentValue = currentValue + 100;
     $(".activities p").text("Total: $ " + currentValue);
-    $("[name='node']").prop('disabled', 'true');
+    $("[name='node']").prop('disabled', 'true').parent().css({"color":"gray"});
   } else {
     currentValue = currentValue - 100;
     $(".activities p").text("Total: $ " + currentValue);
-    $("[name='node']").removeAttr('disabled');
+    $("[name='node']").removeAttr('disabled').parent().css({"color":"black"});
     }
  });
  $("input[name='node']").click(function(){
   if($(this).is(":checked")){
     currentValue = currentValue + 100;
     $(".activities p").text("Total: $ " + currentValue);
-    $("[name='js-libs']").prop('disabled', 'true');
+    $("[name='js-libs']").prop('disabled', 'true').parent().css({"color":"gray"});
   } else {
     currentValue = currentValue - 100;
     $(".activities p").text("Total: $ " + currentValue);
-    $("[name='js-libs']").removeAttr('disabled');
+    $("[name='js-libs']").removeAttr('disabled').parent().css({"color":"black"});
     }
  });
 
@@ -126,9 +126,173 @@ $("input[name='js-frameworks']").click(function(){
   } else {
     currentValue = currentValue - 200;
     $(".activities p").text("Total: $ " + currentValue);
-    }
+   }
   });
-
   
- 
-    
+//  Payment Info
+$('#payment').val('credit card');
+$("p").eq(1).hide();
+$("p").eq(2).hide();
+
+$('#payment').change(function(){
+  if ($('#payment option:selected').val() === 'credit card'){
+    $(".credit-card").show();
+    $(".col-6 col").show();
+    $(".col-3 col").show();
+    $("#exp-month").show();
+    $("#exp-year").show();
+    $("p").eq(1).hide();
+    $("p").eq(2).hide();
+  }
+
+});
+
+$('#payment').change(function(){
+  if ($('#payment option:selected').val() === 'paypal'){
+  $(".credit-card").hide();
+  $(".col-6 col").hide();
+  $(".col-3 col").hide();
+  $("#exp-month").hide();
+  $("#exp-year").hide();
+  $("p").eq(1).show();
+  $("p").eq(2).hide();
+  }
+});
+
+
+$('#payment').change(function(){
+  if ($('#payment option:selected').val() === 'bitcoin'){
+  $(".credit-card").hide();
+  $(".col-6 col").hide();
+  $(".col-3 col").hide();
+  $("#exp-month").hide();
+  $("#exp-year").hide();
+  $("p").eq(1).hide();
+  $("p").eq(2).show();
+  }
+});
+
+$('#payment').change(function(){
+  if ($('#payment option:selected').val() === 'select_method'){
+  $(".credit-card").hide();
+  $(".col-6 col").hide();
+  $(".col-3 col").hide();
+  $("#exp-month").hide();
+  $("#exp-year").hide();
+  $("p").eq(1).hide();
+  $("p").eq(2).hide();
+  }
+});
+
+$(".container").append("<p></p>");
+$(".container").submit(function(){
+  let isFormValid = true;
+  event.preventDefault();
+  $("#name:input").each(function(){
+    if ($.trim($(this).val()).length == 0){
+    $(':button').attr('disabled', false);
+    $(this).addClass("highlight").css("border", "1px solid red");
+    isFormValid = false;
+    $(".container p").text("Please enter your name.");
+  }
+    else{
+     $(':button').attr('disabled',true);
+     $(this).removeClass("highlight");
+     $(this).css("border", "1px solid black");
+        }
+    });
+    return isFormValid;
+});
+
+
+  $(".container").submit(function(){
+    let isFormValid = true;
+    event.preventDefault();
+    $("#mail:input").each(function(){
+        if ($.trim($(this).val()).length == 0){
+            $(this).addClass("highlight").css("border", "1px solid red");
+            isFormValid = false;
+        } else{
+            $(this).removeClass("highlight");
+            $(this).css("border", "1px solid black");
+        }
+    });
+    return isFormValid;
+});
+
+  $('#mail').focusout(function(){
+    $('#mail').filter(function(){
+    var email=$('#mail').val();
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+     if( !emailReg.test( email ) ) {
+     alert('Please enter a valid email.');
+     } 
+      })
+    });
+  
+
+  $(".container").submit(function(){
+    let isFormValid = true;
+    event.preventDefault();
+    $(".activities:input").each(function(){
+        if ($(".activities").not(":checked")){
+            $(this).addClass("highlight").css('color', 'red');
+            isFormValid = false;
+        }
+        else{ 
+            $(this).removeClass("highlight").css("color", "black");
+        }
+    });
+    return isFormValid;
+});
+
+$(".container").submit(function(){
+  let isFormValid = true;
+  event.preventDefault();
+  $("#cc-num:input").each(function(){
+    if ($.trim($(this).val()).length < 13 || ($(this).val()).length > 19){
+        $(this).addClass("highlight").css("border", "1px solid red");
+        isFormValid = false;
+    }
+    else{
+        $(this).removeClass("highlight");
+        
+    }
+});
+return isFormValid;
+});
+  
+    $(".container").submit(function(){
+      let isFormValid = true;
+      event.preventDefault();
+      if ($('#payment option:selected').val() === 'credit card'){ 
+    $("#zip:input").each(function(){
+      if ($.trim($(this).val()).length < 5 || ($(this).val()).length > 5){
+        $(this).addClass("highlight").css("border", "1px solid red");
+        
+        isFormValid = false;
+    } else{
+        $(this).removeClass("highlight");
+    }
+  })
+  }
+return isFormValid;
+});
+  
+    $(".container").submit(function(){
+      let isFormValid = true;
+      event.preventDefault();
+      if ($('#payment option:selected').val() === 'credit card'){ 
+    $("#cvv:input").each(function(){
+      if ($.trim($(this).val()).length < 3 || ($(this).val()).length > 3){
+        $(this).addClass("highlight").css("border", "1px solid red");
+        isFormValid = false;
+    }
+    else{
+        $(this).removeClass("highlight");
+    }
+  })
+  }
+return isFormValid;
+});
+
